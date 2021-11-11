5600e8807efb63d045e4d93c664bbd3aa8ee25b7
sifat-fahimul <mdsifatfeni@gmail.com>
2021-11-11 21:54:36 +0600

    commit

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ const { json } = require('express');
 const port = process.env.PORT || 5000
 
 
-//middleWare
+//middleWaree
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fj83e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
